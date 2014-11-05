ff481daa766e3da2c94e1b4253e14a5468653d33
Joshua Cranmer <Pidgeot18@gmail.com>
2014-11-05 13:28:15 -0600

    Initial, partial support for drawing graphs.

---
+++ graphmaker.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import csv
+import subprocess
+
+class DataSet(object):
+    '''A dataset is a class that contains the data and configuration for a
+    single line in a graph.'''
+
+    color = None
+    title = None
+    xvalues = []
+    yvalues = []
+
+    def __init__(self, xval, yval):
+        self.xvalues = xval
+        self.yvalues = yval
+
+    def xmax(self):
+        return max(self.xvalues)
+
+    def ymax(self):
+        return max(self.yvalues)
+
+    def make_title(self):
+        if not self.title:
+            return "notitle"
+        return "title %s" % repr(self.title)
+
+    def average(self, func):
+        bins = dict()
+        for x, y in zip(self.xvalues, self.yvalues):
+            bins.setdefault(x, []).append(y)
+        data = [(x, func(y)) for (x, y) in bins.iteritems()]
+        data.sort()
+        self.xvalues, self.yvalues = zip(*data)
+
+    def baseline(self, dataset):
+        assert self.xvalues == dataset.xvalues
+        self.yvalues = [sy / float(by)
+            for sy, by in zip(self.yvalues, dataset.yvalues)]
+
+    def clone(self):
+        new = DataSet(list(self.xvalues), list(self.yvalues))
+        new.color = self.color
+        new.title = self.title
+        return new
+
+class DataTable(object):
+    '''A collection of datasets that allows some rudimentary database
+    operations, like select or join.'''
+
+    def __init__(self, datasets):
+        self.data = datasets
+        assert len(self.data) > 0
+        length = len(self.data[0].yvalues)
+        for d in self.data:
+            assert len(d.yvalues) == length
+
+    def select(self, acceptor):
+        cols = [d.yvalues for d in self.data]
+        cols.append(self.data[0].xvalues)
+        newcols = zip(*[row for row in zip(*cols) if acceptor(row)])
+        xvals = newcols.pop()
+        return DataTable([DataSet(xvals, col) for col in newcols])
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+def build_dataset(csvfd, xcol, ycols=None):
+    parse = float
+    reader = csv.reader(csvfd)
+    header = next(reader)
+    columndata = [[] for col in header]
+    for row in reader:
+        map(lambda d: d[0].append(parse(d[1])), zip(columndata, row))
+    if ycols is None:
+        ycols = range(len(columndata))
+    return [DataSet(columndata[xcol], columndata[ycol]) for ycol in ycols]
+
+def make_plot(datasets, outfd):
+    basex = datasets[0].xvalues
+    for ds in datasets:
+        if basex != ds.xvalues:
+            raise Exception('Incompatible datasets')
+    plot = subprocess.Popen('gnuplot', stdin=subprocess.PIPE, stdout=outfd)
+    plot.stdin.write('set term png enhanced\n')
+    plot.stdin.write("set datafile separator ','\n")
+    plot.stdin.write(plot_commands)
+    plot.stdin.write("plot")
+    files = []
+    for ds in datasets:
+        if len(files) > 0:
+            plot.stdin.write(',')
+        plot.stdin.write(" '-'")
+
+        # Add data columns
+        #plot.stdin.write(" using %d:%d" % (len(cols) + 1, len(cols) + 2))
+        files.append([ds.xvalues, ds.yvalues])
+
+        # With the dataset type
+        plot.stdin.write(" with linespoints")
+
+        # Title
+        plot.stdin.write(" " + ds.make_title())
+    plot.stdin.write('\n')
+    for cols in files:
+        for row in zip(*cols):
+            plot.stdin.write(','.join(str(col) for col in row) + '\n')
+        plot.stdin.write('e\n')
+    plot.stdin.close()
+    plot.wait()
+
+def main():
+    global gImageFile
+    parser = OptionParser()
+    parser.add_option('-o', '--output', dest='outfile', default='/dev/stdout',
+        help="Choose the output file to save the image as")
+    (options, args) = parser.parse_args()
+
+    gImageFile = options.outfile
+
+    if len(args) != 1:
+        parser.error("Need exactly one file to read")
+    with open(args[0], 'rt') as fd:
+        exec_file(fd.read(), args[0])
+
+def exec_file(source, path):
+    code = compile(source, path, 'exec')
+    exec(code)
+
+# The following are the commands that we can use from the graph rules file input
+# on the command line.
+plot_commands = ''
+def add_gnuplot_commands(commands):
+    global plot_commands
+    if plot_commands:
+        plot_commands += '\n'
+    plot_commands += commands
+
+def read_csv(filename, xcol):
+    with open(filename, 'r') as fd:
+        return DataTable(build_dataset(fd, xcol))
+
+gImageFile = None
+def plot(datasets):
+    with open(gImageFile, 'w') as fd:
+        make_plot(datasets, fd)
+
+if __name__ == '__main__':
+    main()
